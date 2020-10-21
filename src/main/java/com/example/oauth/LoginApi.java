1d09369605e26b6a086c944b4fec1c3db094d7d2
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-10-21 11:56:09 +0200

    LoginApi

---
+++ src/main/java/com/example/oauth/LoginApi.java
@@ -0,0 +1,25 @@
+package com.example.oauth;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class LoginApi {
+
+    @PostMapping("/logIn")
+    public String login(@RequestBody User user) {
+        long currentTimeMillis = System.currentTimeMillis();
+        return Jwts.builder()
+                .setSubject(user.getLogin())
+                .claim("roles", "user")
+                .setIssuedAt(new Date(currentTimeMillis))
+                .setExpiration(new Date(currentTimeMillis + 20000))
+                .signWith(SignatureAlgorithm.ES512, user.getPassword())
+                .compact();
+    }
+}
